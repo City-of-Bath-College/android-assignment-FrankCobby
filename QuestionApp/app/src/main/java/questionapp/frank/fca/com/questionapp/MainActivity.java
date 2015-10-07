6b8d7d9f561dd8aed9a6ce4cd5ca4245c9418f17
FrankCobby <cob14143419@COBC33357.CityBathColl.ac.uk>
2015-10-07 11:39:24 +0100

    Updated

--- QuestionApp/app/src/main/java/questionapp/frank/fca/com/questionapp/MainActivity.java
+++ QuestionApp/app/src/main/java/questionapp/frank/fca/com/questionapp/MainActivity.java
@@ -4,34 +4,122 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    //variables go here
+    private Button btnFalse;
+    private Button btnTrue;
+    private TextView lblQuestion;
+    private ImageView imgPicture;
+
+    private List<QuestionObject> questions;
+
+    private QuestionObject currentQuestion;
+    private int index;
+
+    private int score;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //connect variables to interface items
+        btnFalse = (Button) findViewById(R.id.btnFalse);
+        btnTrue = (Button) findViewById(R.id.btnTrue);
+        lblQuestion = (TextView) findViewById(R.id.lblQuestion);
+        imgPicture = (ImageView) findViewById(R.id.imgPicture);
+
+
+// set questionnaire text
+        lblQuestion.setText("Is this Spain's official flag?");
+
+// set image picture
+        imgPicture.setImageResource(R.drawable.spainflag);
+
+//on click listeners
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                determineButtonPress(true);
+            }
+        });
+
+//onclick listeners
+        btnFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                determineButtonPress(false);
+            }
+        });
+
+        index = 0;
+        score = 0;
+        generateQuestions();
+
+        setupQuestion();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void generateQuestions(){
+
+        questions = new ArrayList<>();
+
+
+        questions.add(new QuestionObject("Is this Spain's official flag?", true, R.drawable.spainflag));
+        questions.add(new QuestionObject("Is this monument in Rome?", false, R.drawable.paris));
+        questions.add(new QuestionObject("Is New York the capital of USA?", false, R.drawable.usa));
+        questions.add(new QuestionObject("Is this England's flag?", true, R.drawable.england));
+        questions.add(new QuestionObject("His name is Luke Skywalker", false, R.drawable.vader));
+        questions.add(new QuestionObject("This is a giraffe", true, R.drawable.giraffe));
+        questions.add(new QuestionObject("1 pound equals 1.20 Euros", false, R.drawable.pound));
+        questions.add(new QuestionObject("This is Australia", false, R.drawable.africa));
+        questions.add(new QuestionObject("His name is Denzel Washington", false, R.drawable.cage));
+        questions.add(new QuestionObject("Is this Jamaica's flag?", true, R.drawable.jamaica));
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void setupQuestion(){
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        currentQuestion = questions.get(index);
+
+        lblQuestion.setText(currentQuestion.getQuestion());
+        imgPicture.setImageResource(currentQuestion.getPicture());
+
+        index++;
+
+    }
+
+    private void determineButtonPress(boolean answer){
+
+        boolean expectedAnwer = currentQuestion.isAnswer();
+
+        if (answer == expectedAnwer){
+            // You were right!
+
+            Toast.makeText(MainActivity.this, "Well done!", Toast.LENGTH_SHORT).show();
+        } else {
+            // You were wrong!
+
+            Toast.makeText(MainActivity.this, "Wrong answer!", Toast.LENGTH_SHORT).show();
+            score++;
         }
 
-        return super.onOptionsItemSelected(item);
+        setupQuestion();
+
     }
+
+
+
 }
